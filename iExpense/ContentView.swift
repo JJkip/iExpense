83a2ae3dc71de64f2d38a37828e1f674144140e6
jjkip <jjlangatjose@gmail.com>
2023-06-07 18:05:57 +0300

    Showing,hiding views and passing data from one view to another

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,14 +7,33 @@
 
 import SwiftUI
 
+struct SecondView: View {
+    @Environment(\.dismiss) var dismiss
+    let name: String
+    var body: some View{
+        VStack {
+            Text("Hallo: \(name)")
+            Button("Dismiss") {
+                dismiss()
+            }
+        }
+    }
+}
 class User: ObservableObject {
    @Published var firstname = "Arap"
    @Published var lastname = "Ketung"
 }
 struct ContentView: View {
+    @State private var showingSheet = false
     @StateObject var user = User()
     var body: some View {
         VStack {
+            Button("Show second view"){
+                showingSheet.toggle()
+            }
+            .sheet(isPresented: $showingSheet) {
+                SecondView(name: "@twoChains")
+            }
             Text("Your name is: \(user.firstname) \(user.lastname)")
             TextField("First Name", text: $user.firstname)
             TextField("Last Name", text: $user.lastname)
