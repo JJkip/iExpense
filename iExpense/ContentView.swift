d32a43546d83769a4dc29ed09412f07eab1fee56
jjkip <jjlangatjose@gmail.com>
2023-06-08 06:13:07 +0300

    using userdefaults() to store data of the same type e.g integers

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ForthView: View {
-    @State private var tapCount = 1
+    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
     var body: some View{
         Button("Tap count: \(tapCount)"){
             tapCount += 1
